6177be8be154442dc09ce7573c06a7d29dc306c9
sanjidaluna809 <sanjidaluna07@gmail.com>
2025-11-02 06:48:11 +0000

    git commit -m "first commit"

---
+++ labtask.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<head>
+    <title>my form</title>
+</head>
+<center>
+    <h1>Clinic Patient Registration</h1>
+<table>
+    <tr>
+        <td>Full Name:</td></tr>
+        <tr><td><input type ="text"></td>
+</tr>
+<tr>
+    <td>Age:</td></tr>
+    <tr><td><input type="text"></td>
+</tr>
+<tr>
+    <td>Phone Number:</td></tr>
+    <tr><td><input type="text"></td>
+</tr>
+<tr>
+    <td>Email Address:</td></tr>
+    <tr><td><input type="text"></td>
+</tr>
+<tr>
+    <td>Insurance Provider:</td></tr>
+   <tr> <td><select>
+        <option>Select Provider</option>
+        <option>A1</option>
+        <option>B1</option>
+        <option>A2</option>
+        <option>B2</option>
+</select></td>
+</tr>
+<tr>
+    <td>Insurance Policy Number:</td></tr>
+    <tr><td><input type="text"></td>
+</tr>
+    <td><h2>Additional Information</h2></td>
+<tr>
+    <td>Username:</td></tr>
+   <tr> <td><input type="text"></td>
+</tr>
+<tr>
+    <td>Password:</td></tr>
+    <tr><td><input type="text"></td>
+</tr>
+<tr>
+<tr>
+    <td>Confirm Password:</td></tr>
+    <tr><td><input type="text"></td>
+</tr>
+        <tr><td><button type="Submit">Register</button></td></tr>
+
+</table>
+</center>
